095d571004bcaaa090e261ac43da9d5875bf868f
JAE-SEOK AHN <60424424+JAE-SEOK-AHN@users.noreply.github.com>
2020-07-03 23:40:15 +0900

    UPDATE VER 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
+
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : 'Jae-Seok Ahn',
+  'birthday' : '980601',
+  'gender' : 'Male',
+  'job' : 'student' 
+  },
+  {
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : 'Jun-tae Kim',
+  'birthday' : '960102',
+  'gender' : 'Male',
+  'job' : 'baseball player' 
+  },
+  {
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : 'Ji-hoon Choi',
+  'birthday' : '951234',
+  'gender' : 'Male',
+  'job' : 'baseball player' 
+  }
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">     
-        <img src={logo} alt="logo" />
-        <h2>Develop management system!!</h2>
-      </div>
+      <div>
+        {
+          customers.map(c => {return(<Customer id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>)})             
+        }
+        </div>
       );
     }  
   }
